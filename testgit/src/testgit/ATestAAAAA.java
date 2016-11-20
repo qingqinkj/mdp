184aca9be89620e0047c92519377b41394eddf93
cycsir8 <cyc58469@163.com>
2016-11-20 22:08:59 +0800

    test

---
+++ testgit/src/testgit/ATestAAAAA.java
@@ -0,0 +1,14 @@
+package testgit;
+
+public class ATestAAAAA {
+
+	public ATestAAAAA() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
